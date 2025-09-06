0b881786a5187efab69b42cee3abf8612ae85bdb
Krishna-Thakkar <krishnathakkar104@gmail.com>
2025-09-07 03:11:55 +0530

    feat: add skeleton of FlipkartScraper and DataIngestion classes and update requirements.txt

---
+++ prod_assistant/etl/data_ingestion.py
@@ -0,0 +1,52 @@
+import os
+import pandas
+from dotenv import load_dotenv
+from typing import List
+from langchain_core.documents import Document
+from langchain_astradb import AstraDBVectorStore
+from prod_assistant.utils.model_loader import ModelLoader
+from prod_assistant.utils.config_loader import load_config
+
+
+class DataIngestion:
+    """
+    Class to handle data transformation and ingestion into AstraDB vector store.
+    """
+    def __init__(self):
+        pass
+
+    def _load_env_variables(self):
+        """
+        Load environment variables from dotenv file.
+        """
+        pass
+
+    def _get_csv_path(self):
+        """
+        Get the path to the csv file.
+        """ 
+        pass
+
+    def _load_csv(self):
+        """
+        Load the CSV file into a dataframe.
+        """
+        pass
+
+    def transform_data(self):
+        """
+        Tranform the data for ingestion.
+        """
+        pass
+
+    def store_in_vector_db(self):
+        """
+        Store the transformed data into the vector database.
+        """
+        pass
+
+    def run_pipeline(self):
+        """
+        Run the entire data ingestion pipeline.
+        """
+        pass
