77597aeb797c7cb943d6f18a0878364e91c5357b
Krishna-Thakkar <krishnathakkar104@gmail.com>
2025-09-27 16:30:45 +0530

    feat: add evaluation of llm responses

--- prod_assistant/retriever/retrieval.py
+++ prod_assistant/retriever/retrieval.py
@@ -6,6 +6,7 @@ from typing import List
 from langchain_core.documents import Document
 from utils.config_loader import load_config
 from utils.model_loader import ModelLoader
+from evaluation.ragas_eval import evaluate_context_precision, evaluate_response_relevancy
 from dotenv import load_dotenv
 
 
@@ -51,7 +52,7 @@ class Retriever:
                 search_kwargs={"k": top_k,
                                "fetch_k": 20,
                                "lambda_mult": 0.7,
-                               "score_threshold": 0.3
+                               "score_threshold": 0.6
                                })
             print("Retriever loaded successfully")
 
@@ -70,8 +71,34 @@ class Retriever:
 
 if __name__ == "__main__":
     retriever_obj = Retriever()
-    user_query = "Can you suggest good budget laptops?"
-    results = retriever_obj.call_retriever(user_query)
+    user_query = "Can you suggest a good iPhone?"
+    retrieved_docs = retriever_obj.call_retriever(user_query)
 
-    for idx, doc in enumerate(results, 1):
-        print(f"Result {idx}: {doc.page_content}\nMetadata: {doc.metadata}\n")
+    def _format_docs(docs) -> str:
+        if not docs:
+            return "No relevant documents found"
+        formatted_chunks = []
+        for d in docs:
+            meta = d.metadata or {}
+            formatted = (
+                f"Title: {meta.get('product_title', 'N/A')}\n"
+                f"Price: {meta.get('price', 'N/A')}\n"
+                f"Rating: {meta.get('rating', 'N/A')}\n"
+                f"Reviews:\n{d.page_content.strip()}"
+            )
+            formatted_chunks.append(formatted)
+        return "\n\n---\n\n".join(formatted_chunks)
+    
+    retrieved_contexts = [_format_docs(doc) for doc in retrieved_docs]
+
+    response="iphone 16 plus, iphone 16, iphone 15 are best phones under 1,00,000 INR."
+
+    context_score = evaluate_context_precision(user_query, response, retrieved_contexts)
+    relevancy_score = evaluate_response_relevancy(user_query,response,retrieved_contexts)
+    
+    print("\n--- Evaluation Metrics ---")
+    print("Context Precision Score:", context_score)
+    print("Response Relevancy Score:", relevancy_score)
+
+    # for idx, doc in enumerate(results, 1):
+    #     print(f"Result {idx}: {doc.page_content}\nMetadata: {doc.metadata}\n")
