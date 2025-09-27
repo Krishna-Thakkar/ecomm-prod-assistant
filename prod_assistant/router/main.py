94d625da76514cd17144549e01b45294592811a0
Krishna-Thakkar <krishnathakkar104@gmail.com>
2025-09-27 16:44:48 +0530

    feat: change main.py to use agentic workflow with mcp and websearch

--- prod_assistant/router/main.py
+++ prod_assistant/router/main.py
@@ -4,11 +4,11 @@ from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
-
-from workflow.agentic_rag_workflow import AgenticRAG
+from langchain_core.messages import HumanMessage
+from workflow.agentic_workflow_with_mcp_websearch import AgenticRAG
 
 app = FastAPI()
-app.mount("/static", StaticFiles(directory="static", name="static"))
+app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 app.add_middleware(
